7f4cfd438b21ada6a6b180d45fcfed25c5403290
harshat <harshalgupta164@gmail.com>
2023-06-27 13:07:29 +0530

    commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import { ProductReducer, selectedProductReducer } from "./ProductReducer";
+const reducers = combineReducers({
+  allProducts: ProductReducer,
+  product: selectedProductReducer,
+});
+export default reducers;
